95d02c9ddd5a3a91ca2dcd9b9404ec494594f358
legh <cptmail@163.com>
2020-04-04 10:58:56 +0800

    init import

---
+++ src/main/java/com/legh/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.legh.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/test")
+    public String helloTest() {
+        return "Hello world!";
+    }
+}
